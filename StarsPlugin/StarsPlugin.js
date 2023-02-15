808b7a747164c3c7039c77292bf6df6bb9ccf4a9
ESNintex <122349895+ESNintex@users.noreply.github.com>
2023-02-15 15:15:50 +0200

    Update StarsPlugin.js

--- StarsPlugin/StarsPlugin.js
+++ StarsPlugin/StarsPlugin.js
@@ -6,6 +6,23 @@ export class StarsPlugin extends LitElement {
   static properties = {
     slid: {type: Boolean},
   };
+  render() {
+    return html`
+     <div class="rate">
+    
+    <input type="radio" id="star5" name="rate" value="5" @click=${() => this._handleClick(5)} />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="4" @click=${() => this._handleClick(4)}/>
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="3" @click=${() => this._handleClick(3)}/>
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="2" @click=${() => this._handleClick(2)}/>
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="1" @click=${() => this._handleClick(1)}/>
+    <label for="star1" title="text">1 star</label>
+  </div>
+    `;
+  }
   static styles = css`
         *{
             margin: 0;
@@ -77,24 +94,6 @@ _handleClick(e) {
     const event = new CustomEvent('ntx-value-change', args);
     this.dispatchEvent(event);
     console.log(e);
-  }
-  
-  render() {
-    return html`
-     <div class="rate">
-    
-    <input type="radio" id="star5" name="rate" value="5" @click=${() => this._handleClick(5)} />
-    <label for="star5" title="text">5 stars</label>
-    <input type="radio" id="star4" name="rate" value="4" @click=${() => this._handleClick(4)}/>
-    <label for="star4" title="text">4 stars</label>
-    <input type="radio" id="star3" name="rate" value="3" @click=${() => this._handleClick(3)}/>
-    <label for="star3" title="text">3 stars</label>
-    <input type="radio" id="star2" name="rate" value="2" @click=${() => this._handleClick(2)}/>
-    <label for="star2" title="text">2 stars</label>
-    <input type="radio" id="star1" name="rate" value="1" @click=${() => this._handleClick(1)}/>
-    <label for="star1" title="text">1 star</label>
-  </div>
-    `;
   }
 }
 const elementName = 'stars-plugin';
